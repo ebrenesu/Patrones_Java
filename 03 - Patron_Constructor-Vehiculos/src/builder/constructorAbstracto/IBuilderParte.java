babcd2bbe71db6a2da9599607633e3d5a918077a
Erick B (cenfo) <43499120+ebrenesu@users.noreply.github.com>
2024-05-20 21:28:02 -0600

    Delete 03 - Patron_Constructor-Vehiculos directory

--- 03 - Patron_Constructor-Vehiculos/src/builder/constructorAbstracto/IBuilderParte.java
+++
@@ -1,8 +0,0 @@
-package builder.constructorAbstracto;
-
-import builder.productoAbstracto.AParte;
-
-public interface IBuilderParte {
-	public AParte CrearProducto();
-	public AParte ObtenerProducto();
-}
